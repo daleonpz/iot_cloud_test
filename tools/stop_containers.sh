6c990a8c1ac0fb50a5db3210887e71d8854a5c40
daleonpz <daleonpz@gmail.com>
2024-08-14 20:53:59 +0200

    add new tools

--- tools/stop_containers.sh
+++ tools/stop_containers.sh
@@ -2,22 +2,11 @@
 
 # TODO: create containers following a pattern, so I can stop them all at once
 CONTAINERS=$(docker ps -a -q)
-IMAGE_IDS=$(docker images -q)
 
 if [ -z "$CONTAINERS" ]; then
     echo "No containers to stop"
-    exit 0
+else
+    echo "Containers to stop: $CONTAINERS"
+    docker stop $CONTAINERS
+    docker rm $CONTAINERS
 fi
-
-# Stop all containers
-docker stop "$CONTAINERS"
-docker rm "$CONTAINERS"
-
-if [ -z "$IMAGE_IDS" ]; then
-    echo "No images to remove"
-    exit 0
-fi
-
-# Remove all images
-docker rmi -f "$IMAGE_IDS"
-
