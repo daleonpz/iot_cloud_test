e7efc3d2b74ba57c820052999b443aabd702177f
daleonpz <daleonpz@gmail.com>
2024-09-02 18:24:18 +0200

    formatting

--- airflow/dags/transform_data.py
+++ airflow/dags/transform_data.py
@@ -5,25 +5,26 @@ from datetime import datetime
 # from modules import Transformer
 from modules.transformer import Transformer
 
+
 def run_transformations():
     transformer = Transformer()
     transformer.ProcessDataFromDatalake()
     transformer.services.Close()
 
+
 default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': datetime(2023, 7, 27),
-    'retries': 1,
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime(2023, 7, 27),
+    "retries": 1,
 }
 
-dag = DAG('transform_data', default_args=default_args, schedule_interval='@hourly')
+dag = DAG("transform_data", default_args=default_args, schedule_interval="@hourly")
 
 run_transform = PythonOperator(
-    task_id='run_transform',
+    task_id="run_transform",
     python_callable=run_transformations,
     dag=dag,
 )
 
 run_transform
-
